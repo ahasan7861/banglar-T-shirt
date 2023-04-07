b305ce4b8b770e482109544a226bbb673cc90a48
ahasan7861 <ahfahasan786@gmail.com>
2023-04-07 12:31:26 +0600

    Condetional Rendering

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './Cart.css'
 
-const Cart = () => {
+const Cart = ({cart, handleRemoveFromCart}) => {
+    let message;
+    if(cart.length === 0){
+        message = <p>Please add some product</p>
+    }
+    else{
+        message = <div>
+            <h3>Boroloxx!</h3>
+            <p><small>Thanks for spend your money.</small></p>
+        </div>
+    }
     return (
         <div>
-            <h2>Order Summary</h2>
+            <h2>Order Summary: {cart.length}</h2>
+            {message}
+            {
+                cart.map(tshirt => <p key={tshirt._id}>{tshirt.name} <button onClick={()=>handleRemoveFromCart(tshirt._id)}>X</button></p>)
+            }
         </div>
     );
 };
